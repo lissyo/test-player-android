497783e6f07e49e8ae11ebc08b0e32033fa8269c
lissyo <igorekrussia@gmail.com>
2020-04-21 13:50:09 +0300

    Добавлено базовое Активити

---
+++ app/src/main/java/tv/limehd/test_player_android/MainActivity.java
@@ -0,0 +1,27 @@
+package tv.limehd.test_player_android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
